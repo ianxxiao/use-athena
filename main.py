a396079444989d0c82ec74111cbc3bae527bffd0
Ian Xiao <ian.xxiao@gmail.com>
2019-12-12 13:14:11 -0500

    - PEP8 Formatting - add test case.

--- main.py
+++ main.py
@@ -6,59 +6,52 @@ import urllib.parse
 
 class crawler:
 
-	# Initialize the crawler with the name of database
-	def __init__(self, dbname):
-		pass
+    # Initialize the crawler with the name of database
+    def __init__(self, dbname):
+        pass
 
-	def __del__(self):
-		pass
+    def __del__(self):
+        pass
 
-	def db_commit(self):
-		pass
+    def db_commit(self):
+        pass
 
-	# Auxilliary function for getting an entry id (add if it doesn't exist)
-	def get_entry_id(self, table, field, value, create_new = True):
-		return None
+    # Auxilliary function for getting an entry id (add if it doesn't exist)
+    def get_entry_id(self, table, field, value, create_new=True):
+        return None
 
-	# Index an individual page
-	def add_to_index(self, url, soup):
-		print("Indexing %s" % url)
+    # Index an individual page
+    def add_to_index(self, url, soup):
+        print("Indexing %s" % url)
 
-	# Extract the text from an HTML page (no tags)
-	def get_text_only(self, soup):
-		return None
+    # Extract the text from an HTML page (no tags)
+    def get_text_only(self, soup):
+        return None
 
-	# Seperate the words by any non-whitespace character 
-	def seperate_words(self, text):
-		return None
+    # Seperate the words by any non-whitespace character
+    def seperate_words(self, text):
+        return None
 
-	# Return true if this url is alread indexed
-	def is_indexed(self, url):
-		return False
+    # Return true if this url is alread indexed
+    def is_indexed(self, url):
+        return False
 
-	# add a link between two pages
-	def add_link_ref(self, url_From, url_To, link_Text):
-		pass
+    # add a link between two pages
+    def add_link_ref(self, url_From, url_To, link_Text):
+        pass
 
-	# start with a list of pages, do a breadth first search to the given depth, index page as we go
-	def crawl(self, pages, depth = 2):
-		pass
+    # start with a list of pages, do a breadth first search to the given depth, index page as we go
+    def crawl(self, pages, depth=2):
+        pass
 
-	# create the database tables
-	def create_index_tables(self):
-		pass
+    # create the database tables
+    def create_index_tables(self):
+        pass
 
-
-# THIS IS THE MAIN CODE
-
-def cal_multiple(a, b):
-
-    return int(a) * int(b)
 
 def cal_addition(a, b):
-
     return int(a) + int(b)
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     print(">>>>> " + str(cal_addition(2, 4)))

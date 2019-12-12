9a80cdea5a1d38c0d3227ecacffbae3d638ef294
Ian Xiao <ian.xxiao@gmail.com>
2019-12-12 14:49:32 -0500

    - turn warning off. - change expected links to 10000

--- test_search_engine.py
+++ test_search_engine.py
@@ -1,6 +1,8 @@
 import pytest
+import warnings
+warnings.filterwarnings("ignore")
 
-@pytest.mark.parametrize("pagelist, num_links", [(['https://en.wikipedia.org/wiki/Star_Wars'], 10)])
+@pytest.mark.parametrize("pagelist, num_links", [(['https://en.wikipedia.org/wiki/Star_Wars'], 10000)])
 def test_crawler(pagelist, num_links):
     # test if the crawler can get at least 10 links from the Star War Wiki Page
     from search_engine import crawler

6b33f57c4b7bdb8c35c480eb7ed8457c009f5946
Ian Xiao <ian.xxiao@gmail.com>
2019-12-12 13:22:46 -0500

    - fix dummy test case.

--- test_main_pytest.py
+++ test_main_pytest.py
@@ -1,6 +1,6 @@
 from main import cal_addition
 import pytest
 
-@pytest.mark.parametrize("a, b, expected", [(2, 3, 5), (3, 5, 8), (5, 8, 13), (10, 10, 20), (20, 20, 23)])
+@pytest.mark.parametrize("a, b, expected", [(2, 3, 5), (3, 5, 8), (5, 8, 13), (10, 10, 20), (20, 20, 40)])
 def test_addition(a, b, expected):
     assert cal_addition(a, b) == expected

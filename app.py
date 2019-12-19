4e0a29b412e361a933a267a5c974d5bf660083d8
ian-xxiao <ian.xxiao@gmail.com>
2019-12-19 00:42:22 -0500

    - re architected helper.db - added new table of USER_QUERY to capture emails and look up terms

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
+from configs import search_engine_config
+import sqlite3
+from helper.db import get_db
+
+DATABASE = search_engine_config.TEST_DB_NAME
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    cur = get_db(DATABASE)
     return render_template("index.html")
 
 
@@ -18,6 +24,13 @@ def success():
         print(email)
         print(idea_1, idea_2, idea_3)
     return render_template("success.html")
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    cur = getattr(g, '_database', None)
+    if cur is not None:
+        cur.close()
 
 
 if __name__ == '__main__':
---
+++ helper/db.py
@@ -0,0 +1,40 @@
+# This script includes all helper functions to manage SQLite DB
+import sqlite3
+import os
+
+
+def get_db(DATABASE):
+    # stand up a db if it does not exist
+    if os.path.isfile(DATABASE) is False:
+        try:
+            conn = sqlite3.connect(DATABASE)
+            create_index_tables(conn)
+        except:
+            print(">>>>> Cannot Create a new SQL DB. Terminate. <<<<<")
+    else:
+        try:
+            conn = sqlite3.connect(DATABASE)
+        except:
+            print(">>>>> Something is Wrong. Cannot Connect to DB. <<<<<<")
+
+    return conn.cursor()
+
+
+def create_index_tables(conn):
+
+    # create tables
+    conn.execute('create table URL_LIST(url)')
+    conn.execute('create table WORD_LIST(word)')
+    conn.execute('create table WORD_LOCATION(url_id, word_id, location)')
+    conn.execute('create table LINK(from_id integer, to_id integer)')
+    conn.execute('create table LINK_WORDS(word_id, link_id)')
+    conn.execute('create table USER_QUERY(email, query)')
+
+    # index the tables
+    conn.execute('create index word_idx on WORD_LIST(word)')
+    conn.execute('create index url_idx on URL_LIST(url)')
+    conn.execute('create index word_url_idx on WORD_LIST(word)')
+    conn.execute('create index url_to_idx on LINK(to_id)')
+    conn.execute('create index url_from_idx on LINK(from_id)')
+    conn.execute('create index email_idx on USER_QUERY(email)')
+    conn.commit()

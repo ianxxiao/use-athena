fb054f3533b4c83b6865868c2b5398bbe2d3a5fa
Ian Xiao <ian.xxiao@gmail.com>
2019-12-19 19:06:41 -0500

    - modifying pytest script to test

--- tests/functional/test_search_engine.py
+++ tests/functional/test_search_engine.py
@@ -1,6 +1,5 @@
 import sys
 sys.path.append('../use-athena')
-
 import pytest
 from configs import search_engine_config
 from helper import clean_db
@@ -9,14 +8,12 @@ import os
 from search_engine import Crawler
 import mechanize
 
-page_list = ['https://en.wikipedia.org/wiki/James_Bond', 'https://en.wikipedia.org/wiki/Star_Wars',
-             'https://towardsdatascience.com/data-science-is-boring-1d43473e353e']
-
 
 @pytest.mark.parametrize("email, ideas, db_name", [("ian.xxiao@gmail.com",
                                                     ["my 1st idea", "my 2nd idea", "my 3rd idea"],
                                                     search_engine_config.TEST_DB_NAME)])
 def test_app_idea_entry(email, ideas, db_name):
+    # https://www.patricksoftwareblog.com/testing-a-flask-application-using-pytest/
 
     # clean the db
     clean_db.clean_test_db()
@@ -41,27 +38,27 @@ def test_app_idea_entry(email, ideas, db_name):
     res = conn.execute("select url from URL_LIST").fetchall()
     assert len(res) == len(ideas)
 
-
-@pytest.mark.parametrize("pages, db_name", [(page_list, search_engine_config.TEST_DB_NAME)])
-def test_crawler(pages, db_name):
-    clean_db.clean_test_db()
-    crawler = Crawler(db_name)
-    crawler.crawl(pages, depth=1)
-    conn = sqlite3.connect(db_name)
-
-    # test if db set up works properly
-    assert os.path.isfile(db_name)
-
-    # test if all urls are indexed properly
-    res = conn.execute("select url from URL_LIST").fetchall()
-    assert len(res) == len(page_list)
-
-    # test if word counts are sensible
-    res = conn.execute("select count(*) from WORD_LIST").fetchall()
-    assert res[0][0] >= 20000
-
-    # test if URL-WORD is indexed properly
-    res = conn.execute("select DISTINCT(url_id) from WORD_LOCATION").fetchall()
-    assert len(res) == len(page_list)
-
-    crawler.__del__()
+#
+# @pytest.mark.parametrize("pages, db_name", [(page_list, search_engine_config.TEST_DB_NAME)])
+# def test_crawler(pages, db_name):
+#     clean_db.clean_test_db()
+#     crawler = Crawler(db_name)
+#     crawler.crawl(pages, depth=1)
+#     conn = sqlite3.connect(db_name)
+#
+#     # test if db set up works properly
+#     assert os.path.isfile(db_name)
+#
+#     # test if all urls are indexed properly
+#     res = conn.execute("select url from URL_LIST").fetchall()
+#     assert len(res) == len(page_list)
+#
+#     # test if word counts are sensible
+#     res = conn.execute("select count(*) from WORD_LIST").fetchall()
+#     assert res[0][0] >= 20000
+#
+#     # test if URL-WORD is indexed properly
+#     res = conn.execute("select DISTINCT(url_id) from WORD_LOCATION").fetchall()
+#     assert len(res) == len(page_list)
+#
+#     crawler.__del__()

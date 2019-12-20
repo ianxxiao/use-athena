908cc2ca24970582dce06b4760202d46a6d557cf
ian-xxiao <ian.xxiao@gmail.com>
2019-12-20 10:17:28 -0500

    - add unit test case for score ideas

---
+++ tests/unit/test_analytics.py
@@ -0,0 +1,17 @@
+# this tests the analytics functions
+import sys
+
+sys.path.append('../use-athena')
+import pytest
+from helper.analytics import score_ideas
+
+
+@pytest.mark.parametrize("ideas", [["love is great", "love is boring", "love is crazy"]])
+def test_db_user_query(ideas):
+
+    ranked_ideas = score_ideas(ideas)
+
+    assert ranked_ideas[0][1] > ranked_ideas[1][1]
+    assert ranked_ideas[0][1] > ranked_ideas[2][1]
+    assert ranked_ideas[0][1] <= 100
+    assert ranked_ideas[2][1] >= 0

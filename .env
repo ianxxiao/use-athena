908ecc3953718fcfa7b5d1dfd2c7530bde7d84b9
ian-xxiao <ian.xxiao@gmail.com>
2020-01-07 23:52:48 -0500

    untrack env file; re-factor to remove secret keys

---
+++ .env
@@ -0,0 +1,2 @@
+FROM_EMAIL = "athena.analytics.tech@gmail.com"
+EMAIL_PW = "athena@123"
--- athena/__init__.py
+++ athena/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from athena.helper.db import create_table
+import os
 
+# setup env vars
+if os.path.exists(".env"):
+    print("found environment var, setting up ...")
+    from dotenv import load_dotenv
+    load_dotenv()
+
+# start up the flask app
 flask_app = Flask(__name__)
+
+# set up the db tables
 create_table()
+
 # app.config['SQLALCHEMY_DATABASE_URI'] = ''
 # db = SQLAlchemy
 
